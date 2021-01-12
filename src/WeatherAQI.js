c6b55ee634c6ae4767b8a8621454bbf01a785fc5
rozenlior <72037051+rozenlior@users.noreply.github.com>
2021-01-12 15:41:37 -0800

    Added airDesc conditional

--- src/WeatherAQI.js
+++ src/WeatherAQI.js
@@ -4,18 +4,32 @@ import axios from "axios";
 export default function WeatherAQI(props) {
 const[loaded, setLoaded]= useState(false);
 const[aqi, setAqi]= useState(null);
+const[airDesc, setAirDesc]= useState("");
+
 
 function handleAqi(response){
 setAqi(response.data.data[0].aqi);
 console.log(response.data.data[0].aqi);
+setAirDesc(response.data.data[0].aqi);
 setLoaded(true);
-
 }
+  if (aqi >= 301) {
+    airDesc= "Hazardous";
+  } else if ((aqi >= 201) & (aqi < 301)) {
+    airDesc= "Very Unhealthy";
+  } else if ((aqi >= 101) & (aqi < 201)) {
+    airDesc= "Unhealthy";
+  } else if ((aqi >= 51) & (aqi < 101)) {
+   airDesc=  "Moderate";
+  } else if ((aqi >= 0) & (aqi < 51)) {
+    airDesc= "Good";
+  }
+
 
 if(loaded) {
 return(
   <div className="WeatherAQI">
-   <strong>AQI:</strong> {aqi}
+   <strong>AQI:</strong> {aqi}({airDesc})
     </div>
 );
 } else {
